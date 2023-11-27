f853b6d43c64a3c551ec85475f69f89187a253c2
Michael Goodness <mike@opsgoodness.net>
2023-11-27 11:38:41 -0600

    chore: update gconfig function

--- dot_config/fish/functions/gconfig.fish
+++ dot_config/fish/functions/gconfig.fish
@@ -3,9 +3,20 @@ function gconfig
         set -f fzf_query "--query=$argv"
     end
 
-    set -l context (gcloud config configurations list --format='value(name)' | fzf --no-multi $fzf_query)
+    set -l project (gcloud projects list --format='table(name,project_id)' | fzf --header-lines=1 --no-multi $fzf_query)
 
-    if test -n "$context"
-        gcloud config configurations activate "$context"
+    if test -n $project
+        set -l context $(string split ' ' -f1 $project)
+        set -l contexts $(gcloud config configurations list --format='value(name)')
+        if not contains $context $contexts
+            gcloud config configurations create $context
+        else
+            gcloud config configurations activate $context
+        end
+
+        gcloud config set core/project $(string split ' ' -f2 -m1 -r $project)
+        gcloud config set core/account michael.goodness@mlb.com
+        gcloud config set compute/region us-central1
+        gcloud config set compute/zone us-central1-a
     end
 end
