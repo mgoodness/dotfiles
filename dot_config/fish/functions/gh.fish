a950aebcd2f91c33b3056c8011561284dee5c264
Michael Goodness <mike@opsgoodness.net>
2025-06-04 11:18:01 -0500

    feat: add EMU config

--- dot_config/fish/functions/gh.fish
+++ dot_config/fish/functions/gh.fish
@@ -1,10 +1,15 @@
 function gh --wraps=gh
     if test \( "$argv[1]" = repo -a "$argv[2]" = clone \) -a -n "$argv[3]"
-        switch $(string split / "$argv[3]" | count)
+        switch (string split / "$argv[3]" | count)
             case 1
                 set repo_dir "$GIT_WORKSPACE/github.com/mgoodness/$argv[3]"
             case 2
-                set repo_dir "$GIT_WORKSPACE/github.com/$argv[3]"
+                set org_repo "$argv[3]"
+                if test ( string sub -l 4 "$org_repo" ) = "mlb-"
+                    set repo_dir "$GIT_WORKSPACE/emu.github.com/$org_repo"
+                else
+                    set repo_dir "$GIT_WORKSPACE/github.com/$org_repo"
+                end
             case 3
                 set repo_dir "$GIT_WORKSPACE/$argv[3]"
         end
