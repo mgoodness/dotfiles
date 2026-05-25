a6433aee8ab5d35d4004799e29557b36d03569a6
Michael Goodness <mike@opsgoodness.net>
2026-05-25 16:45:37 -0500

    Revert "chore(helix): remove helix editor"
    This reverts commit 4cb30e08e6337b646225db4e80d1902bc2be2ca0.

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 status is-login || exit
 
 # Environment
-set -gx EDITOR "zed --wait"
+set -gx EDITOR hx
 
 set -gx GIT_MERGE_AUTOEDIT no # accept default merge commit message
 set -gx GIT_WORKSPACE ~/Code
